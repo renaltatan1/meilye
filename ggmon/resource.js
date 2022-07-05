febf52c52617c633a069f6514633bc6f16fd19bd
renaltatan <53061575+renaltatan@users.noreply.github.com>
2022-07-05 15:02:49 -0500

    Update resource.js

--- ggmon/resource.js
+++ ggmon/resource.js
@@ -21,16 +21,16 @@ var dynamicLoading = {
     type: 't2022022201',
     time: '1649663375'
 }
-dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/Metro/style.css')
-dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/Metro/player.css')
-dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/Metro/phoneTemplate.css')
-dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/Metro/template.css')
-dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/Metro/hiSlider2.min.css')
-dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/Metro/FlipBookPlugins.min.css')
-dynamicLoading.js('https://renaltatan.github.io/meilye/ggmon/Metro/jquery-3.5.1.min.js', function () {
-    dynamicLoading.js('https://renaltatan.github.io/meilye/ggmon/Metro/main.js', function () {
+dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/metro/style.css')
+dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/metro/player.css')
+dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/metro/phoneTemplate.css')
+dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/metro/template.css')
+dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/metro/hiSlider2.min.css')
+dynamicLoading.css('https://renaltatan.github.io/meilye/ggmon/metro/FlipBookPlugins.min.css')
+dynamicLoading.js('https://renaltatan.github.io/meilye/ggmon/metro/jquery-3.5.1.min.js', function () {
+    dynamicLoading.js('https://renaltatan.github.io/meilye/ggmon/metro/main.js', function () {
     })
-    dynamicLoading.js('https://renaltatan.github.io/meilye/ggmon/Metro/visitinfo.js')
-    dynamicLoading.js('https://renaltatan.github.io/meilye/ggmon/Metro/flipHtml5.hiSlider2.min.js')
-    dynamicLoading.js('https://renaltatan.github.io/meilye/ggmon/Metro/FlipBookPlugins.min.js')
+    dynamicLoading.js('https://renaltatan.github.io/meilye/ggmon/metro/visitinfo.js')
+    dynamicLoading.js('https://renaltatan.github.io/meilye/ggmon/metro/flipHtml5.hiSlider2.min.js')
+    dynamicLoading.js('https://renaltatan.github.io/meilye/ggmon/metro/FlipBookPlugins.min.js')
 })
